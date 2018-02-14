85ba2181610d7bf599a480d2a344f66061089b74
Tomas Bauer <mail@tbauer.eu>
2018-02-14 14:04:41 +0100

    Comment fix

--- test/eu/tbauer/codewars/XOTest.java
+++ test/eu/tbauer/codewars/XOTest.java
@@ -9,8 +9,6 @@ import org.junit.Test;
  * Check to see if a string has the same amount of 'x's and 'o's. The method
  * must return a boolean and be case insensitive. The string can contain any
  * char.
- *
- * @author Tomas Bauer
  */
 public class XOTest {
 	@Test

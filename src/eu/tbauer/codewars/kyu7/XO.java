b4e421f77fcc930e5036c8dda23cb250d6473df0
Tomas Bauer <mail@tbauer.eu>
2018-02-14 14:42:01 +0100

    Comment fix

--- src/eu/tbauer/codewars/kyu7/XO.java
+++ src/eu/tbauer/codewars/kyu7/XO.java
@@ -9,6 +9,8 @@ package eu.tbauer.codewars.kyu7;
  * Check to see if a string has the same amount of 'x's and 'o's. The method
  * must return a boolean and be case insensitive. The string can contain any
  * char.
+ * 
+ * https://www.codewars.com/kata/55908aad6620c066bc00002a
  *
  * @author Tomas Bauer
  */
